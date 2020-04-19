91861b1f40b92007408a51ed1989311da53c0122
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-04-19 13:57:48 +0900

    コメントを追加

--- domain/spotify/auth.go
+++ domain/spotify/auth.go
@@ -2,6 +2,7 @@ package spotify
 
 import "golang.org/x/oauth2"
 
+// Auth はSpotify OAuthに関連したAPIを呼び出すためのインターフェイスです。
 type Auth interface {
 	GetAuthURL(state string) string
 	Exchange(code string) (*oauth2.Token, error)
