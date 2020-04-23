36ff96337e562b3926fdf6c299bef4c36a22ed5b
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-04-24 00:45:01 +0900

    コメントを追加

--- mysql/schemas/relaym/users.sql
+++ mysql/schemas/relaym/users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `users` (
-  `id` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT 'ユーザID',
-  `spotify_user_id` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT 'SpotifyのユーザID',
-  `display_name` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT '表示名',
+  `id` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT 'ユーザID (不変)',
+  `spotify_user_id` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT 'SpotifyのユーザID (不変)',
+  `display_name` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT '表示名 (変更可能)',
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_spotify_user_id_uindex` (`spotify_user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
