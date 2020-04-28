f724e9de682bc28202a53e152ba9f738fc55e487
sanposhiho <handbomusic@gmail.com>
2020-04-29 00:48:19 +0900

    fix: add auth scopes

--- spotify/client.go
+++ spotify/client.go
@@ -16,7 +16,7 @@ type Client struct {
 
 // NewClient はClientのポインタを生成する関数です。
 func NewClient(cfg *config.Spotify) *Client {
-	auth := spotify.NewAuthenticator(cfg.RedirectURL(), spotify.ScopeUserReadPrivate)
+	auth := spotify.NewAuthenticator(cfg.RedirectURL(), spotify.ScopeUserReadPrivate, spotify.ScopeUserReadPlaybackState)
 	auth.SetAuthInfo(cfg.ClientID(), cfg.ClientSecret())
 	return &Client{auth: auth}
 }
