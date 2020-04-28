04f3db62c929bda525919400fe8039f546e75734
sanposhiho <handbomusic@gmail.com>
2020-04-28 16:44:55 +0900

    fix: rename track to trackCli

--- usecase/track.go
+++ usecase/track.go
@@ -9,15 +9,15 @@ import (
 
 // TrackUseCase は音楽に関するユースケースです。
 type TrackUseCase struct {
-	track spotify.TrackClient
+	trackCli spotify.TrackClient
 }
 
 // NewTrackUseCase はTrackUseCaseのポインタを生成します。
 func NewTrackUseCase(track spotify.TrackClient) *TrackUseCase {
-	return &TrackUseCase{track: track}
+	return &TrackUseCase{trackCli: track}
 }
 
 // SearchTracks はクエリから音楽を検索します。
 func (t *TrackUseCase) SearckTracks(ctx context.Context, q string) ([]*entity.Track, error) {
-	return t.track.Search(ctx, q)
+	return t.trackCli.Search(ctx, q)
 }
