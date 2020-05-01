4d837ea8b524ed63c2b06bf0fad135bbc5ed7b4e
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-05-01 22:12:24 +0900

    不要はSleepを削除

--- web/handler/websocket.go
+++ web/handler/websocket.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/camphor-/relaym-server/domain/entity"
 	"github.com/camphor-/relaym-server/domain/event"
@@ -44,8 +43,6 @@ func (h *WebSocketHandler) WebSocket(c echo.Context) error {
 	h.hub.Register(wsCli)
 
 	go wsCli.PushLoop()
-
-	time.Sleep(100 * time.Millisecond)
 
 	// TODO テスト用に置いとくだけで後で消す
 	h.hub.Push(&event.PushMessage{
