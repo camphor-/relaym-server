29a6b541d5731657c2ec7fe1ff870659665b9c57
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-05-04 15:33:46 +0900

    アクセストークンのリフレッシュを行うSpotify APIを実装

--- spotify/client.go
+++ spotify/client.go
@@ -35,3 +35,13 @@ func (c *Client) Exchange(code string) (*oauth2.Token, error) {
 	}
 	return token, nil
 }
+
+// Refresh はリフレッシュトークンを使用して新しいアクセストークンを取得します。
+func (c *Client) Refresh(token *oauth2.Token) (*oauth2.Token, error) {
+	cli := c.auth.NewClient(token)
+	newToken, err := cli.Token()
+	if err != nil {
+		return nil, fmt.Errorf("token refresh: %w", err)
+	}
+	return newToken, nil
+}
