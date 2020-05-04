46ae1136715d0049bdc08c1a3758368474da5567
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-05-05 01:12:44 +0900

    SpotifyのPlayer APIを操作するクライアントを実装

---
+++ spotify/player.go
@@ -0,0 +1,127 @@
+package spotify
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/camphor-/relaym-server/domain/entity"
+	"github.com/camphor-/relaym-server/domain/service"
+
+	"github.com/zmb3/spotify"
+)
+
+// CurrentlyPlaying は現在の再生状況を取得するAPIです。
+// TODO : 現状はなんの情報が必要か分かってないので現在再生中かどうかのみ返します。
+func (c *Client) CurrentlyPlaying(ctx context.Context) (bool, error) {
+	token, ok := service.GetTokenFromContext(ctx)
+	if !ok {
+		return false, errors.New("token not found")
+	}
+	cli := c.auth.NewClient(token)
+	cp, err := cli.PlayerCurrentlyPlaying()
+	if convErr := c.convertPlayerError(err); convErr != nil {
+		return false, fmt.Errorf("spotify api: currently playing: %w", convErr)
+	}
+	return cp.Playing, nil
+}
+
+// Play は曲を再生し始めるか現在再生途中の曲の再生を再開するAPIです。
+// APIが非同期で処理がされるため、リクエストが返ってきても再生が開始しているとは限りません。
+// 設定が反映されたか確認するには CurrentlyPlaying() を叩く必要があります。
+func (c *Client) Play(ctx context.Context) error {
+	token, ok := service.GetTokenFromContext(ctx)
+	if !ok {
+		return errors.New("token not found")
+	}
+	cli := c.auth.NewClient(token)
+
+	// TODO : デバイスIDを指定する必要がある場合はいじる
+	opt := &spotify.PlayOptions{DeviceID: nil}
+	err := cli.PlayOpt(opt)
+	if convErr := c.convertPlayerError(err); convErr != nil {
+		return fmt.Errorf("spotify api: play or resume: %w", convErr)
+	}
+	return nil
+}
+
+// Pause は再生を一時停止します。
+// APIが非同期で処理がされるため、リクエストが返ってきても再生が一時停止されているとは限りません。
+// 設定が反映されたか確認するには CurrentlyPlaying() を叩く必要があります。
+func (c *Client) Pause(ctx context.Context) error {
+	token, ok := service.GetTokenFromContext(ctx)
+	if !ok {
+		return errors.New("token not found")
+	}
+	cli := c.auth.NewClient(token)
+
+	// TODO : デバイスIDを指定する必要がある場合はいじる
+	opt := &spotify.PlayOptions{DeviceID: nil}
+	err := cli.PauseOpt(opt)
+	if convErr := c.convertPlayerError(err); convErr != nil {
+		return fmt.Errorf("spotify api: pause: %w", convErr)
+	}
+	return nil
+}
+
+// AddToQueue は曲を「次に再生される曲」に追加するAPIです。
+// APIが非同期で処理がされるため、リクエストが返ってきても曲の追加が完了しているとは限りません。
+// 設定が反映されたか確認するには CurrentlyPlaying() を叩く必要があります。
+func (c *Client) AddToQueue(ctx context.Context, trackID string) error {
+	token, ok := service.GetTokenFromContext(ctx)
+	if !ok {
+		return errors.New("token not found")
+	}
+	cli := c.auth.NewClient(token)
+
+	// TODO : デバイスIDを指定する必要がある場合はいじる
+	opt := &spotify.PlayOptions{DeviceID: nil}
+	err := cli.QueueSongOpt(spotify.ID(trackID), opt)
+	if convErr := c.convertPlayerError(err); convErr != nil {
+		return fmt.Errorf("spotify api: add queue: %w", convErr)
+	}
+	return nil
+}
+
+// SetRepeatMode はリピートモードの設定を変更するAPIです。
+// APIが非同期で処理がされるため、リクエストが返ってきてもリピートモードの設定が完了しているとは限りません。
+// 設定が反映されたか確認するには CurrentlyPlaying() を叩く必要があります。
+func (c *Client) SetRepeatMode(ctx context.Context, on bool) error {
+	token, ok := service.GetTokenFromContext(ctx)
+	if !ok {
+		return errors.New("token not found")
+	}
+	cli := c.auth.NewClient(token)
+
+	state := "off"
+	if on {
+		state = "context"
+	}
+
+	// TODO : デバイスIDを指定する必要がある場合はいじる
+	opt := &spotify.PlayOptions{DeviceID: nil}
+
+	if err := cli.RepeatOpt(state, opt); c.convertPlayerError(err) != nil {
+		return fmt.Errorf("spotify api: set repeat mode: %w", c.convertPlayerError(err))
+	}
+	return nil
+}
+
+func (c *Client) convertPlayerError(err error) error {
+	if e, ok := err.(spotify.Error); ok {
+		switch {
+		case e.Status == http.StatusForbidden && strings.Contains(e.Message, "Restriction violated"):
+			// https://github.com/spotify/web-api/issues/1205
+			fmt.Printf("already in the ideal state: %s\n", e.Message)
+			return nil
+		case e.Status == http.StatusForbidden:
+			return fmt.Errorf("%s: %w", e.Message, entity.ErrNonPremium)
+
+		case e.Status == http.StatusNotFound:
+			return fmt.Errorf("%s: %w", e.Message, entity.ErrActiveDeviceNotFound)
+		}
+	}
+	return err
+}
---
+++ spotify/player_test.go
@@ -0,0 +1,195 @@
+// +build integration
+
+package spotify
+
+import (
+	"context"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/camphor-/relaym-server/domain/service"
+	"golang.org/x/oauth2"
+
+	"github.com/camphor-/relaym-server/config"
+)
+
+func TestClient_CurrentlyPlaying(t *testing.T) {
+	tests := []struct {
+		name    string
+		want    bool
+		wantErr bool
+	}{
+		{
+			name:    "再生中ではないときfalseが変える",
+			want:    false,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := NewClient(config.NewSpotify())
+			token := &oauth2.Token{
+				AccessToken:  "",
+				TokenType:    "Bearer",
+				RefreshToken: os.Getenv("SPOTIFY_REFRESH_TOKEN_FOR_TEST"),
+				Expiry:       time.Now(),
+			}
+			token, err := c.Refresh(token)
+			if err != nil {
+				t.Fatal(err)
+			}
+			ctx := context.Background()
+			ctx = service.SetTokenToContext(ctx, token)
+
+			got, err := c.CurrentlyPlaying(ctx)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CurrentlyPlaying() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("CurrentlyPlaying() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestClient_Play(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{
+			name:    "現在の曲が再生される",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := NewClient(config.NewSpotify())
+			token := &oauth2.Token{
+				AccessToken:  "",
+				TokenType:    "Bearer",
+				RefreshToken: os.Getenv("SPOTIFY_REFRESH_TOKEN_FOR_TEST"),
+				Expiry:       time.Now(),
+			}
+			token, err := c.Refresh(token)
+			if err != nil {
+				t.Fatal(err)
+			}
+			ctx := context.Background()
+			ctx = service.SetTokenToContext(ctx, token)
+
+			if err := c.Play(ctx); (err != nil) != tt.wantErr {
+				t.Errorf("Play() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestClient_Pause(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{
+			name:    "曲が一時停止される",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := NewClient(config.NewSpotify())
+			token := &oauth2.Token{
+				AccessToken:  "",
+				TokenType:    "Bearer",
+				RefreshToken: os.Getenv("SPOTIFY_REFRESH_TOKEN_FOR_TEST"),
+				Expiry:       time.Now(),
+			}
+			token, err := c.Refresh(token)
+			if err != nil {
+				t.Fatal(err)
+			}
+			ctx := context.Background()
+			ctx = service.SetTokenToContext(ctx, token)
+			if err := c.Pause(ctx); (err != nil) != tt.wantErr {
+				t.Errorf("Pause() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestClient_AddToQueue(t *testing.T) {
+
+	tests := []struct {
+		name    string
+		trackID string
+		wantErr bool
+	}{
+		{
+			name:    "曲をqueueに追加できる",
+			trackID: "49BRCNV7E94s7Q2FUhhT3w", // uriではなくidなので"spotify:track:"はいらない
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := NewClient(config.NewSpotify())
+			token := &oauth2.Token{
+				AccessToken:  "",
+				TokenType:    "Bearer",
+				RefreshToken: os.Getenv("SPOTIFY_REFRESH_TOKEN_FOR_TEST"),
+				Expiry:       time.Now(),
+			}
+			token, err := c.Refresh(token)
+			if err != nil {
+				t.Fatal(err)
+			}
+			ctx := context.Background()
+			ctx = service.SetTokenToContext(ctx, token)
+
+			if err := c.AddToQueue(ctx, tt.trackID); (err != nil) != tt.wantErr {
+				t.Errorf("AddToQueue() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestClient_SetRepeatMode(t *testing.T) {
+	tests := []struct {
+		name    string
+		on      bool
+		wantErr bool
+	}{
+		{
+			name:    "リピートモードをオフにできる",
+			on:      false,
+			wantErr: false,
+		},
+		{
+			name:    "リピートモードをオンにできる",
+			on:      true,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := NewClient(config.NewSpotify())
+			token := &oauth2.Token{
+				AccessToken:  "",
+				TokenType:    "Bearer",
+				RefreshToken: os.Getenv("SPOTIFY_REFRESH_TOKEN_FOR_TEST"),
+				Expiry:       time.Now(),
+			}
+			token, err := c.Refresh(token)
+			if err != nil {
+				t.Fatal(err)
+			}
+			ctx := context.Background()
+			ctx = service.SetTokenToContext(ctx, token)
+			if err := c.SetRepeatMode(ctx, tt.on); (err != nil) != tt.wantErr {
+				t.Errorf("SetRepeatMode() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
