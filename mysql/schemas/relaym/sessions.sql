37432d223225656a84cc10d739b15ba493cee653
sanposhiho <handbomusic@gmail.com>
2020-05-07 13:41:23 +0900

    add: add sessions and tracks

---
+++ mysql/schemas/relaym/sessions.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `id` VARCHAR(255) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NOT NULL COMMENT 'セッションID（不変）',
+  `name` VARCHAR(255) NOT NULL COMMENT 'Sessionの名前（不変）',
+  `creator_id` VARCHAR(255) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NOT NULL COMMENT 'sessionの作成者のユーザーID（不変）',
+  `queue_head` INT NOT NULL COMMENT 'プレイヤーにセットされている曲のindex（0-indexed）（可変）',
+  PRIMARY KEY (`id`),
+  INDEX `sessions_user_id_fk_idx` (`creator_id` ASC) VISIBLE,
+  CONSTRAINT `sessions_user_id_fk`
+    FOREIGN KEY (`creator_id`)
+    REFERENCES `users` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
---
+++ mysql/schemas/relaym/tracks.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS `tracks` (
+  `index` INT NOT NULL COMMENT 'session内でのindex（0-indexed）（不変）',
+  `uri` VARCHAR(255) NOT NULL COMMENT 'Spotify APIから返ってくるuri（不変）',
+  `session_id` VARCHAR(255) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NOT NULL COMMENT 'Trackが属するsessionのID（不変）',
+  INDEX `tracks_session_id_fk_idx` (`session_id` ASC) VISIBLE,
+  CONSTRAINT `tracks_session_id_fk`
+    FOREIGN KEY (`session_id`)
+    REFERENCES `sessions` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
