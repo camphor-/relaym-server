5d60173ab6c8b9b9400914d22b96a7d54c5b5fcb
sanposhiho <handbomusic@gmail.com>
2020-05-08 17:09:43 +0900

    fix: rename table and create unique

--- mysql/schemas/relaym/queue_tracks.sql
+++ mysql/schemas/relaym/queue_tracks.sql
@@ -1,8 +1,9 @@
-CREATE TABLE IF NOT EXISTS `tracks` (
+CREATE TABLE IF NOT EXISTS `queue_tracks` (
   `index` INT NOT NULL COMMENT 'session内でのindex（0-indexed）（不変）',
   `uri` VARCHAR(255) NOT NULL COMMENT 'Spotify APIから返ってくるuri（不変）',
-  `session_id` VARCHAR(255) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NOT NULL COMMENT 'Trackが属するsessionのID（不変）',
+  `session_id` VARCHAR(255) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NOT NULL,
   INDEX `tracks_session_id_fk_idx` (`session_id` ASC) VISIBLE,
+  UNIQUE INDEX `queue_tracks_index_uri_uindex` (`index` ASC, `uri` ASC) VISIBLE,
   CONSTRAINT `tracks_session_id_fk`
     FOREIGN KEY (`session_id`)
     REFERENCES `sessions` (`id`)
