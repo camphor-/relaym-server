48b2c7e0c0245394e5258a9bfa5e240db5a26f31
sanposhiho <handbomusic@gmail.com>
2020-05-09 16:40:37 +0900

    add: database/session.go

---
+++ database/session.go
@@ -0,0 +1,70 @@
+package database
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"github.com/camphor-/relaym-server/domain/entity"
+	"github.com/camphor-/relaym-server/domain/repository"
+
+	"github.com/go-gorp/gorp/v3"
+	"github.com/go-sql-driver/mysql"
+)
+
+var _ repository.Session = &SessionRepository{}
+
+// SessionRepository は repository.SessionRepository を満たす構造体です
+type SessionRepository struct {
+	dbMap *gorp.DbMap
+}
+
+// NewSessionRepository はSessionRepositoryのポインタを生成する関数です
+func NewSessionRepository(dbMap *gorp.DbMap) *SessionRepository {
+	dbMap.AddTableWithName(sessionDTO{}, "sessions")
+	return &SessionRepository{dbMap: dbMap}
+}
+
+// FindByID は指定されたIDを持つsessionをDBから取得します
+func (r *SessionRepository) FindByID(id string) (*entity.Session, error) {
+	var dto sessionDTO
+	if err := r.dbMap.SelectOne(&dto, "SELECT id, name, creator_id, queue_head, state_type FROM sessions WHERE id = ?", id); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, fmt.Errorf("select session: %w", entity.ErrSessionNotFound)
+		}
+		return nil, fmt.Errorf("select session: %w", err)
+	}
+	return &entity.Session{
+		ID:        dto.ID,
+		Name:      dto.name,
+		CreatorID: dto.creator_id,
+		QueueHead: dto.queue_head,
+		StateType: dto.state_type,
+	}, nil
+}
+
+func (r *SessionRepository) Store(session *entity.Session) error {
+	dto := &sessionDTO{
+		ID:         session.ID,
+		name:       session.Name,
+		creator_id: session.CreatorID,
+		queue_head: session.QueueHead,
+		state_type: session.StateType,
+	}
+
+	if err := r.dbMap.Insert(dto); err != nil {
+		if mysqlErr, ok := err.(*mysql.MySQLError); ok && mysqlErr.Number == 1062 {
+			return fmt.Errorf("insert session: %w", entity.ErrSessionAlreadyExisted)
+		}
+		return fmt.Errorf("insert session: %w", err)
+	}
+	return nil
+}
+
+type sessionDTO struct {
+	ID         string `db:"id"`
+	name       string `db:"name"`
+	creator_id string `db:"creator_id"`
+	queue_head int    `db:"queue_head"`
+	state_type string `db:"state_type"`
+}
