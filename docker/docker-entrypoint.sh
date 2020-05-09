7c7d7fc3f58a0ed83e8b76369a52b0d40ec1926f
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-05-09 16:27:09 +0900

    relaym-serverを起動するDocker関係のファイルを追加

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# マイグレーション
+TMPDIR="/tmp"
+TAR_FILE="/tmp/skeema.tar.gz"
+RELEASES_URL="https://github.com/skeema/skeema/releases"
+VERSION=1.4.2
+curl -s -L -o "${TAR_FILE}" "${RELEASES_URL}/download/v${VERSION}/skeema_${VERSION}_linux_amd64.tar.gz"
+tar -xf "${TAR_FILE}" -C "${TMPDIR}"
+chmod +x "${TMPDIR}/skeema"
+mv "${TMPDIR}/skeema" /usr/local/bin/skeema
+
+cd /mysql
+skeema push -p"${DB_PASSWORD}" "${ENV}"
+
+exec "$@"
