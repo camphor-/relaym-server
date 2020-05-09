ac15f4c878973eba66ba887001265312439e73ad
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-05-09 19:33:19 +0900

    補足コメントを追加

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -11,6 +11,7 @@ chmod +x "${TMPDIR}/skeema"
 mv "${TMPDIR}/skeema" /usr/local/bin/skeema
 
 cd /mysql
+# データベース名に環境のsuffixをつける
 sed -i "s/schema=relaym/schema=relaym_${ENV}/" ./schemas/relaym/.skeema
 skeema push -p"${DB_PASSWORD}" "${ENV}"
 
