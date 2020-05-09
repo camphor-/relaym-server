54d8241ff463e1689ffa4a9227382001373111fb
sanposhiho <handbomusic@gmail.com>
2020-05-09 14:09:25 +0900

    fix: rename and fix order

--- mysql/schemas/relaym/queue_tracks.sql
+++ mysql/schemas/relaym/queue_tracks.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `queue_tracks` (
   `uri` VARCHAR(255) NOT NULL COMMENT 'Spotify APIから返ってくるuri（不変）',
   `session_id` VARCHAR(255) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NOT NULL,
   INDEX `tracks_session_id_fk_idx` (`session_id` ASC) VISIBLE,
-  UNIQUE INDEX `queue_tracks_index_uri_uindex` (`index` ASC, `session_id` ASC) VISIBLE,
+  UNIQUE INDEX `queue_tracks_index_session_id_uindex` (`session_id` ASC, `index` ASC) VISIBLE,
   CONSTRAINT `tracks_session_id_fk`
     FOREIGN KEY (`session_id`)
     REFERENCES `sessions` (`id`)
