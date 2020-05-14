3e515aeb05db9b6121fa4af2cbefd38fc9cb22fc
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-05-15 00:28:26 +0900

    Eventの構造体を作成

--- domain/entity/event.go
+++ domain/entity/event.go
@@ -4,3 +4,22 @@ package entity
 type Event struct {
 	Type string `json:"type"`
 }
+
+var (
+	// EventPlay はセッションの再生が開始された際に発されるイベントです。
+	EventPlay = &Event{
+		Type: "PLAY",
+	}
+
+	// EventNextTrack はセッションの曲の再生が (正常に) 次の曲に移った際に発されるイベント。
+	// キューの現在再生している曲の位置が含まれます。
+	EventNextTrack = &Event{
+		Type: "NEXTTRACK",
+	}
+
+	// EventInterrupt はSpotifyの本体アプリ側で操作されて、Relaym側との同期が取れなくなったタイミングで発されるイベントです。
+	// セッションは一時停止状態になり、RESUMEを送ることで再開されます。
+	EventInterrupt = &Event{
+		Type: "INTERRUPT",
+	}
+)
