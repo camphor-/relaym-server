22751985ee1deda64942c13d11699ee2c276d506
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-05-18 22:36:57 +0900

    ユーザのアップデートメソッドをrepositoryに追加

--- database/user_test.go
+++ database/user_test.go
@@ -175,3 +175,76 @@ func TestUserRepository_Store(t *testing.T) {
 		})
 	}
 }
+
+func TestUserRepository_Update(t *testing.T) {
+	// Prepare
+	dbMap, err := NewDB()
+	if err != nil {
+		t.Fatal(err)
+	}
+	dbMap.AddTableWithName(sessionDTO{}, "sessions")
+	dbMap.AddTableWithName(userDTO{}, "users")
+	truncateTable(t, dbMap)
+	user := &userDTO{
+		ID:            "existing_user",
+		SpotifyUserID: "existing_user_spotify",
+		DisplayName:   "existing_user_display_name",
+		DeviceID:      "existing_device_id",
+	}
+	sameFieldUser := &userDTO{
+		ID:            "user_id",
+		SpotifyUserID: "user_spotify_id",
+		DisplayName:   "display_name",
+		DeviceID:      "device_id",
+	}
+	if err := dbMap.Insert(user, sameFieldUser); err != nil {
+		t.Fatal(err)
+	}
+
+	tests := []struct {
+		name    string
+		user    *entity.User
+		wantErr bool
+	}{
+		{
+			name: "既に存在するユーザの情報を更新できる",
+			user: &entity.User{
+				ID:            "existing_user",
+				SpotifyUserID: "update_existing_user_spotify",
+				DisplayName:   "update_existing_user_display_name",
+				DeviceID:      "update_existing_device_id",
+			},
+			wantErr: false,
+		},
+		{
+			name: "フィールドの値が全てDBの値を一致するユーザで更新してもエラーにならない",
+			user: &entity.User{
+				ID:            "user_id",
+				SpotifyUserID: "user_spotify_id",
+				DisplayName:   "display_name",
+				DeviceID:      "device_id",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := NewUserRepository(dbMap)
+			if err := r.Update(tt.user); (err != nil) != tt.wantErr {
+				t.Errorf("Update() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if !tt.wantErr {
+				got, err := r.FindByID(tt.user.ID)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if !cmp.Equal(tt.user, got) {
+					t.Errorf("Update() diff = %v", cmp.Diff(got, tt.user))
+
+				}
+			}
+		})
+	}
+}
