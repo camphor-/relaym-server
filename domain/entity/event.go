200e7458bae8a2ffb8788188ab5504283780ff01
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-05-26 19:01:27 +0900

    STOPイベントを追加

--- domain/entity/event.go
+++ domain/entity/event.go
@@ -17,6 +17,11 @@ var (
 		Type: "NEXTTRACK",
 	}
 
+	// EventStop は全ての曲の再生が終了した際に発されるイベントです。
+	EventStop = &Event{
+		Type: "STOP",
+	}
+
 	// EventInterrupt はSpotifyの本体アプリ側で操作されて、Relaym側との同期が取れなくなったタイミングで発されるイベントです。
 	// セッションは一時停止状態になり、RESUMEを送ることで再開されます。
 	EventInterrupt = &Event{
