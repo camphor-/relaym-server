b06c5731669dd2d33605b025b0238fa98257ee5a
sanposhiho <handbomusic@gmail.com>
2020-07-23 08:05:11 +0900

    add: add NewQueueTrackToStore

--- domain/entity/queue_track.go
+++ domain/entity/queue_track.go
@@ -1,7 +1,10 @@
 package entity
 
+import "github.com/google/uuid"
+
 // QueueTrackToStore はsessionに属するqueue内に曲を挿入する際に使用します
 type QueueTrackToStore struct {
+	ID        string
 	URI       string
 	SessionID string
 }
@@ -11,4 +14,13 @@ type QueueTrack struct {
 	Index     int
 	URI       string
 	SessionID string
+}
+
+// NewQueueTrackToStore はQueueTrackToStateのポインタを生成する関数です。
+func NewQueueTrackToStore(uri string, sessionID string) *QueueTrackToStore {
+	return &QueueTrackToStore{
+		ID:        uuid.New().String(),
+		URI:       uri,
+		SessionID: sessionID,
+	}
 }
