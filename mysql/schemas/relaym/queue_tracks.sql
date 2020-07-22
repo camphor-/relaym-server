0e0fb18dd615501b551bf7d3eb3969025350aed3
sanposhiho <handbomusic@gmail.com>
2020-07-23 07:31:44 +0900

    fix: add id and rm unuse index

--- mysql/schemas/relaym/queue_tracks.sql
+++ mysql/schemas/relaym/queue_tracks.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS `queue_tracks` (
+  `id` VARCHAR(255) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NOT NULL COMMENT 'ID（不変）',
   `index` INT NOT NULL COMMENT 'session内でのindex（0-indexed）（不変）',
   `uri` VARCHAR(255) NOT NULL COMMENT 'Spotify APIから返ってくるuri（不変）',
   `session_id` VARCHAR(255) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NOT NULL,
-  INDEX `tracks_session_id_fk_idx` (`session_id` ASC) VISIBLE,
+  PRIMARY KEY (`id`),
   UNIQUE INDEX `queue_tracks_index_session_id_uindex` (`session_id` ASC, `index` ASC) VISIBLE,
   CONSTRAINT `tracks_session_id_fk`
     FOREIGN KEY (`session_id`)
