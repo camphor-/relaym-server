985b524a86ca100b78c30887e1ea28de71e21087
sanposhiho <handbomusic@gmail.com>
2020-07-23 12:48:14 +0900

    fix: add pk

--- mysql/schemas/relaym/queue_tracks.sql
+++ mysql/schemas/relaym/queue_tracks.sql
@@ -2,8 +2,7 @@ CREATE TABLE IF NOT EXISTS `queue_tracks` (
   `index` INT NOT NULL COMMENT 'session内でのindex（0-indexed）（不変）',
   `uri` VARCHAR(255) NOT NULL COMMENT 'Spotify APIから返ってくるuri（不変）',
   `session_id` VARCHAR(255) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NOT NULL,
-  INDEX `tracks_session_id_fk_idx` (`session_id` ASC) VISIBLE,
-  UNIQUE INDEX `queue_tracks_index_session_id_uindex` (`session_id` ASC, `index` ASC) VISIBLE,
+  PRIMARY KEY (`session_id`, `index`),
   CONSTRAINT `tracks_session_id_fk`
     FOREIGN KEY (`session_id`)
     REFERENCES `sessions` (`id`)
