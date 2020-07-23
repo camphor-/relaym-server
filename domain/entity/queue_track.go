8643a4999f3186e00b1818b8e9af08f713efa985
sanposhiho <handbomusic@gmail.com>
2020-07-23 12:42:33 +0900

    Revert "add: add NewQueueTrackToStore"
    This reverts commit b06c5731669dd2d33605b025b0238fa98257ee5a.

--- domain/entity/queue_track.go
+++ domain/entity/queue_track.go
@@ -1,10 +1,7 @@
 package entity
 
-import "github.com/google/uuid"
-
 // QueueTrackToStore はsessionに属するqueue内に曲を挿入する際に使用します
 type QueueTrackToStore struct {
-	ID        string
 	URI       string
 	SessionID string
 }
@@ -14,13 +11,4 @@ type QueueTrack struct {
 	Index     int
 	URI       string
 	SessionID string
-}
-
-// NewQueueTrackToStore はQueueTrackToStateのポインタを生成する関数です。
-func NewQueueTrackToStore(uri string, sessionID string) *QueueTrackToStore {
-	return &QueueTrackToStore{
-		ID:        uuid.New().String(),
-		URI:       uri,
-		SessionID: sessionID,
-	}
 }
