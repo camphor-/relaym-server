5466692bfbd7ba7eb8034805b6e3b772b9265329
sanposhiho <handbomusic@gmail.com>
2020-07-23 12:42:34 +0900

    Revert "fix: add id and rm unuse index"
    This reverts commit 0e0fb18dd615501b551bf7d3eb3969025350aed3.

--- mysql/schemas/relaym/queue_tracks.sql
+++ mysql/schemas/relaym/queue_tracks.sql
@@ -1,9 +1,8 @@
 CREATE TABLE IF NOT EXISTS `queue_tracks` (
-  `id` VARCHAR(255) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NOT NULL COMMENT 'ID（不変）',
   `index` INT NOT NULL COMMENT 'session内でのindex（0-indexed）（不変）',
   `uri` VARCHAR(255) NOT NULL COMMENT 'Spotify APIから返ってくるuri（不変）',
   `session_id` VARCHAR(255) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NOT NULL,
-  PRIMARY KEY (`id`),
+  INDEX `tracks_session_id_fk_idx` (`session_id` ASC) VISIBLE,
   UNIQUE INDEX `queue_tracks_index_session_id_uindex` (`session_id` ASC, `index` ASC) VISIBLE,
   CONSTRAINT `tracks_session_id_fk`
     FOREIGN KEY (`session_id`)
